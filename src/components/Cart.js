9076b250cbbd90e458ce8a792c9c27dc394cb9c3
ali <abenheni@e2r13p1.1337.ma>
2023-01-09 22:51:05 +0100

    first commit

---
+++ src/components/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../styles/index.scss"
+
+export default function Cart() {
+  return (
+    <div className='cart'>
+        <h1>Cart</h1>
+        
+    </div>
+  )
+}
---
+++ src/data/plantList.js
@@ -0,0 +1,55 @@
+export  const plantList = [
+	{
+		name: 'monstera',
+		category: 'classic',
+		id: '1ed',
+        isBestSale: true
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classic',
+		id: '2ab'
+	},
+	{
+		name: 'silver pothos',
+		category: 'classic',
+		id: '3sd',
+        isBestSale: true
+	},
+	{
+		name: 'yucca',
+		category: 'classic',
+		id: '4kk',
+        isBestSale: false
+	},
+	{
+		name: 'olive tree',
+		category: 'exterior',
+		id: '5pl',
+        isBestSale: true
+	},
+	{
+		name: 'geranium',
+		category: 'exterior',
+		id: '6uo',
+        isBestSale: true
+	},
+	{
+		name: 'basil',
+		category: 'exterior',
+		id: '7ie',
+        isBestSale: false
+	},
+	{
+		name: 'aloe',
+		category: 'succulent',
+		id: '8fp',
+        isBestSale: false
+	},
+	{
+		name: 'succulent',
+		category: 'succulent',
+		id: '9vn',
+        isBestSale: true
+	}
+]
